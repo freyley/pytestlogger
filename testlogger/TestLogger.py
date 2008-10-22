11c1027e34d612209567fe5814894124f228b8d5
jeff <jeff@ventus.(none)>
2008-10-22 16:36:56 -0700

    copied the assert and fail methods from unittest

--- testlogger/TestLogger.py
+++ testlogger/TestLogger.py
@@ -37,36 +37,57 @@ class TestLogger(unittest.TestCase):
             codeline = self.reportLine(inspect.stack())
             self.logger('FAILED: ' + ae.message + ' in ' + codeline)
 
-    def assertEqual(self,  first,  second,  msg=None):
+    def fail(self, msg=None):
         def func():
-            super(TestLogger,  self).assertEqual(first,  second,  msg)
+            super(TestLogger,  self).fail( msg)
         self.logAssert(func)
-            
-    def assertAlmostEqual(self,  first,  second,  places=6,  msg=None):
-        def func():
-            super(TestLogger,  self).assertAlmostEqual(first,  second,  places,  msg)
-        self.logAssert(func)
-'''        
- 'assertAlmostEquals',
- 'assertEqual',
- 'assertEquals',
- 'assertFalse',
- 'assertNotAlmostEqual',
- 'assertNotAlmostEquals',
- 'assertNotEqual',
- 'assertNotEquals',
- 'assertRaises',
- 'assertTrue',
- 'assert_',
-'fail',
- 'failIf',
- 'failIfAlmostEqual',
- 'failIfEqual',
- 'failUnless',
- 'failUnlessAlmostEqual',
- 'failUnlessEqual',
- 'failUnlessRaises',
- 'failureException',
- #function.func_code.co_varnames gets the argument names
 
-'''
+    def failIf(self, expr, msg=None):
+        def func():
+            super(TestLogger,  self).failIf(expr,  msg)
+        self.logAssert(func)
+
+    def failUnless(self, expr, msg=None):
+        def func():
+            super(TestLogger,  self).failUnless(self,  expr,  msg)
+        self.logAssert(func)
+
+    def failUnlessRaises(self, excClass, callableObj, *args, **kwargs):
+        def func():
+            super(TestLogger,  self).failUnlessRaises(excClass,  callableObj,  *args,  **kwargs)
+        self.logAssert(func)
+
+    def failUnlessEqual(self, first, second, msg=None):
+        def func():
+            super(TestLogger,  self).failUnlessEqual(first,  second,  msg)
+        self.logAssert(func)
+
+    def failIfEqual(self, first, second, msg=None):
+        def func():
+            super(TestLogger,  self).failIfEqual(first,  second,  msg)
+        self.logAssert(func)
+
+    def failUnlessAlmostEqual(self, first, second, places=7, msg=None):
+        def func():
+            super(TestLogger,  self).failUnlessAlmostEqual(first,  second,  places,  msg)
+        self.logAssert(func)
+
+    def failIfAlmostEqual(self, first, second, places=7, msg=None):
+        def func():
+            super(TestLogger,  self).failIfAlmostEqual(first,  second,  places,  msg)
+        self.logAssert(func)
+    # Synonyms for assertion methods
+
+    assertEqual = assertEquals = failUnlessEqual
+
+    assertNotEqual = assertNotEquals = failIfEqual
+
+    assertAlmostEqual = assertAlmostEquals = failUnlessAlmostEqual
+
+    assertNotAlmostEqual = assertNotAlmostEquals = failIfAlmostEqual
+
+    assertRaises = failUnlessRaises
+
+    assert_ = assertTrue = failUnless
+
+    assertFalse = failIf
