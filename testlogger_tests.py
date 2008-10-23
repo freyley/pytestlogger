098932217fb42fedbbb40f8297ef13e048e310b2
jeff <jeff@ventus.(none)>
2008-10-23 12:28:12 -0700

    moved the mainline tests into this branch

--- testlogger_tests.py
+++ testlogger_tests.py
@@ -1,22 +1,33 @@
 import unittest
 from testlogger import testlog
 from StringIO import StringIO
+import inspect,  sys
 
 class TestLoggerTests(unittest.TestCase):
     def setUp(self):
         testlog.reset()
         self.log = StringIO()
+        testlog.log_to_file(self.log)
 
     def testFailuresGoToFile(self):
-        testlog.log_to_file(self.log)
         testlog.assertEqual(4, 3)
-        self.assertEqual(self.log.getvalue(), u'FAILED: 4 != 3 in testlogger_tests.py line 12: testlog.assertEqual(4, 3)\n')
+        answer = u'FAILED: 4 != 3 in testlogger_tests.py line '+str(inspect.getlineno(sys._getframe()) -1)+ ': testlog.assertEqual(4, 3)\n'
+        self.assertEqual(self.log.getvalue(), answer)
         
     def testPassesGoToFile(self):
-        testlog.log_to_file(self.log)
         testlog.logSuccesses = True
         testlog.assertEqual(4, 4)
-        self.assertEqual(self.log.getvalue(),  u'PASSED: testlogger_tests.py line 18: testlog.assertEqual(4, 4)\n')
+        answer = u'PASSED: testlogger_tests.py line '+str(inspect.getlineno(sys._getframe()) -1)+': testlog.assertEqual(4, 4)\n'
+        self.assertEqual(self.log.getvalue(),  answer)
+        testlog.assertEqual(4, 3)
+        answer += u'FAILED: 4 != 3 in testlogger_tests.py line '+str(inspect.getlineno(sys._getframe()) -1)+ ': testlog.assertEqual(4, 3)\n'
+        self.assertEqual(self.log.getvalue(), answer)
+        
+
+    def testAllFunctions(self):
+        testlog.assertTrue(1 == 2)
+        answer = u'FAILED:  in testlogger_tests.py line '+str(inspect.getlineno(sys._getframe()) -1)+': testlog.assertTrue(1 == 2)\n'
+        self.assertEqual(self.log.getvalue(),  answer)
 
 if __name__ == '__main__':
     unittest.main()
